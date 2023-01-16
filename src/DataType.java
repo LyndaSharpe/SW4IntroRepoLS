48412427baeabc464f2943c12beebbee0e1f6380
Lynda Sharpe <lynda_sharpe@btinternet.com>
2023-01-16 21:24:57 +0000

    Third Time Lucky

---
+++ src/DataType.java
@@ -0,0 +1,20 @@
+public class DataType {
+    public static void main(String[] args) {
+        // data type
+        // object = pet cat, pet type, age, weight, name
+        // datatype variable_name = value;
+
+        String petType = "Cat"; // String = text value
+        String name = "Brownie"; //Characters longer than one
+        int petAge = 3; // int = integer = whole number
+        double petWeight = 1.5; //double = fraction
+
+        // call your variables back and print
+        System.out.println("The pet type is " + petType);
+        System.out.println("The pet name is " + name);
+        System.out.println("The pet weight is " + petWeight + " kg and the age is " + petAge + " months");
+
+        // create an object, declare at least 3 variables using different data types and print
+
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+public class HelloWorld { //main class
+    public static void main(String[] args) { //"main" shortcut
+//START HERE
+        System.out.println ("Hello, this is my first program in INTELLIJ IDEA");
+        //sout shortcut
+        System.out.println("Printing a tree");
+        System.out.println("       *       ");
+        System.out.println("      ***      ");
+        System.out.println("     *****     ");
+        System.out.println("    *******    ");
+        System.out.println("       *       ");
+
+    }
+}
